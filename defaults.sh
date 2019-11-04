c8901506fce1618ac7b39fb0f0689a2f33b1c2d6
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-11-03 21:28:25 -0700

    add option services home-assistant and mqtt.

--- defaults.sh
+++ defaults.sh
@@ -8,6 +8,7 @@
 
 # Leave blank to disable this service by default.
 set_service_flag $service_name
+
 #set_service_flag $service_name yes
 
 # Sets the application prefix depending on what $INGRESS_TYPE is set to. 
@@ -16,11 +17,8 @@ set_service_flag $service_name
 # https://prefix.some.domain/
 # prefix=$service_name
 
-# This is how to optionally include additional .yml files. See the prometheus repo 
-# for a complete example.
-## If the include exporters flag is set
-#if [ ! -z "$SERVICE_prometheus_exporters" ]; then
-#    export SERVICE_CONFIG_prometheus_exporters=${TECHNOCORE_SERVICES}/prometheus/exporters.yml
-#fi
+set_optional_service home-assistant
+set_optional_service mqtt
 
-#generate_mount dev shell-migrations /usr/share/dogfish/shell-migrations
+#generate_mount dev shell-migrations /usr/share/dogfish/shell-migr
+#      - ${esphome_live_mount:-./empty/:/opt/.dummy}
