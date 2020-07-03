46c94e5c8230cfd0c4bf48f741863ef796df9ef7
Spencer Hachmeister <tms@spencerslab.com>
2020-07-03 16:28:21 -0600

    Add MQTT support to DosageSwitch

--- esphome/components/dosage/switch/dosage_switch.cpp
+++ esphome/components/dosage/switch/dosage_switch.cpp
@@ -11,24 +11,34 @@ void DosageSwitch::dump_config() { LOG_SWITCH("", "Dosage Switch", this); }
 void DosageSwitch::setup() {
   this->GPIOSwitch::setup();
 
+
+  this->topic_dose_for_seconds = this->get_default_topic_for("dose_for_seconds", this->component_type(), this->get_default_object_id());
+  this->topic_dose_for_ms = this->get_default_topic_for("dose_for_ms", this->component_type(), this->get_default_object_id());
+  this->topic_set_dosage = this->get_default_topic_for("set_dosage", this->component_type(), this->get_default_object_id());
+  this->topic_dose = this->get_default_topic_for("dose", this->component_type(), this->get_default_object_id());
+
+  subscribe(this->topic_dose_for_seconds, &DosageSwitch::on_dose_for_seconds);
+  subscribe(this->topic_dose_for_ms, &DosageSwitch::on_dose_for_ms);
+  subscribe(this->topic_set_dosage, &DosageSwitch::on_set_dosage);
+  subscribe(this->topic_dose, &DosageSwitch::on_dose);
 }
 
-void DosageSwitch::dose_for_ms(uint32_t *time_in_ms) {
-  ESP_LOGD(TAG,"DosageSwitch::dose_for_ms()");
+void DosageSwitch::dose_for_ms(uint32_t time_in_ms) {
+  ESP_LOGI(TAG,"Dosing for %d ms");
   this->turn_on();
-  delay(*time_in_ms);
+  delay(time_in_ms);
   this->turn_off();
 }
 
-void DosageSwitch::dose_for_seconds(uint32_t *time_in_seconds) {
+void DosageSwitch::dose_for_seconds(uint32_t time_in_seconds) {
   uint32_t time_in_ms;
-  time_in_ms = *time_in_seconds * 1000;
-  this->dose_for_ms(&time_in_ms);
+  time_in_ms = time_in_seconds * 1000;
+  this->dose_for_ms(time_in_ms);
 }
 
 void DosageSwitch::dose(){
   ESP_LOGD(TAG,"Dosing: %d", this->dosage_);
-  this->dose_for_ms((&this->dosage_));
+  this->dose_for_ms((this->dosage_));
 }
 
 }  // namespace dosage
--- esphome/components/dosage/switch/dosage_switch.h
+++ esphome/components/dosage/switch/dosage_switch.h
@@ -7,18 +7,32 @@
 namespace esphome {
 namespace dosage {
 
-class DosageSwitch : public gpio::GPIOSwitch { //, public Component {
+class DosageSwitch : public gpio::GPIOSwitch, public mqtt::CustomMQTTDevice {
  public:
   void set_dosage(uint32_t dosage) { this->dosage_ = dosage; };
-  void dose_for_ms(uint32_t *time_in_ms) ;
-  void dose_for_seconds(uint32_t *time_in_seconds) ;
+  void dose_for_ms(uint32_t time_in_ms) ;
+  void dose_for_seconds(uint32_t time_in_seconds) ;
   void dose();
+
+  void on_dose_for_ms(const std::string &topic, const std::string &payload) { this->dose_for_ms(uint32_t(atoi(payload.c_str()))); }
+  void on_dose_for_seconds(const std::string &topic, const std::string &payload) { this->dose_for_seconds(uint32_t(atoi(payload.c_str()))); }
+  void on_set_dosage(const std::string &topic, const std::string &payload) { 
+    ESP_LOGI(TAG,"Updating dosage to be %d ms",payload.c_str());
+    this->set_dosage(uint32_t(atoi(payload.c_str()))); 
+  }
+  void on_dose(const std::string &topic, const std::string &payload) { this->dose(); }
 
   void setup() override;
   void dump_config() override;
+  std::string component_type() const { return "switch"; }
+  std::string get_default_object_id() const { return sanitize_string_whitelist(to_lowercase_underscore(this->get_name()), HOSTNAME_CHARACTER_WHITELIST); }
 
  protected:
   uint32_t dosage_;
+  std::string topic_dose_for_seconds;
+  std::string topic_dose_for_ms;
+  std::string topic_set_dosage;
+  std::string topic_dose;
 };
 
 }  // namespace dosage
